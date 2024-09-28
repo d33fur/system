150ab892bdd24e25b55b9f250c263d680702b601
d33fur <d33fur@gmail.com>
2024-09-28 14:28:50 +0300

    docker fixes

--- install/hello.sh
+++ install/hello.sh
@@ -113,12 +113,12 @@ sudo apt install -y \
     ca-certificates \
     curl \
     software-properties-common \
-    lsb-release \
-    gnupg
+    gnupg \
+    lsb-release
 
 curl -fsSL https://download.docker.com/linux/ubuntu/gpg | \
-    sudo gpg --dearmor -o /usr/share/keyrings/docker-archive-keyring.gpg
-echo "deb [arch=amd64 signed-by=/usr/share/keyrings/docker-archive-keyring.gpg] \
+    sudo gpg --dearmor -o /usr/share/keyrings/docker.gpg
+echo "deb [arch=$(dpkg --print-architecture) signed-by=/usr/share/keyrings/docker.gpg] \
     https://download.docker.com/linux/ubuntu $(lsb_release -cs) stable" | \
     sudo tee /etc/apt/sources.list.d/docker.list > /dev/null
 
@@ -126,16 +126,16 @@ sudo apt update
 sudo apt install -y \
     docker-ce \
     docker-ce-cli \
-    containerd.io
+    containerd.io \
+    docker-compose-plugin \
+    docker-compose
+
+sudo usermod -aG docker $USER
 
 sudo systemctl enable docker
 sudo systemctl start docker
 sudo docker run hello-world
 
-sudo curl -L "https://github.com/docker/compose/releases/latest/download/docker-compose-$(uname -s)-$(uname -m)" \
-    -o /usr/local/bin/docker-compose
-sudo chmod +x /usr/local/bin/docker-compose
-sudo ln -s /usr/local/bin/docker-compose /usr/bin/docker-compose
 docker-compose --version
 
 # 
@@ -155,7 +155,7 @@ sudo apt install -y \
 curl -fSsL https://repo.yandex.ru/yandex-browser/YANDEX-BROWSER-KEY.GPG | \
     sudo gpg --dearmor | \
     sudo tee /usr/share/keyrings/yandex.gpg
-echo "deb [arch=amd64 signed-by=/usr/share/keyrings/yandex.gpg] \
+echo "deb [arch=$(dpkg --print-architecture) signed-by=/usr/share/keyrings/yandex.gpg] \
     http://repo.yandex.ru/yandex-browser/deb stable main" | \
     sudo tee /etc/apt/sources.list.d/yandex-stable.list
 
